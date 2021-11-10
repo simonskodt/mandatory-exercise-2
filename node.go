24ed2fb504540dc5a9472cc1827e776889f761a3
Gustav Metnik-Beck <gustav.mb@outlook.com>
2021-11-10 10:01:36 +0100

    Partly implemented node

--- node.go
+++ node.go
@@ -1,15 +1,34 @@
 package main
 
 import (
+	"flag"
+	"github.com/hashicorp/serf/serf"
 	"google.golang.org/grpc"
-	"mandatory-exercise-2/utils"
 	"mandatory-exercise-2/service"
+	"mandatory-exercise-2/utils"
+	"net"
 )
 
-var lamport = &utils.Lamport{}
-var logger = utils.NewLogger("Node")
+var Node *node
+
+type node struct {
+	id int
+	address string
+	port string
+	client service.ServiceClient
+	server service.ServiceServer
+	lamport *utils.Lamport
+	logger *utils.Logger
+}
 
 func main() {
+	var id = flag.Int("id", 0, "The id of the node.") // Remember to input unique id
+	var address = flag.String("address", "localhost", "The ip address of the node.")
+	var port = flag.String("port", "8080", "The port of the node.")
+	flag.Parse()
+
+
+	Node = newNode(*id, *address, *port)
 	conn, err := grpc.Dial("localhost:8080", grpc.WithInsecure())
 	if err != nil {
 		logger.ErrorLogger.Fatalf("Could not connect to server. :: %v", err)
@@ -17,10 +36,40 @@ func main() {
 	var _ = service.NewServiceClient(conn)
 }
 
-func broadcast() {
+func (n *node) broadcast() {
+
+}
+
+func (n *node) respondClient()  {
 	
 }
 
-func respondClient()  {
-	
+func newNode(id int, address string, port string) *node {
+	return &node{
+		id: id,
+		address: address,
+		port: port,
+		server: newServer(port),
+		client: newClient(address, port),
+		lamport: &utils.Lamport{T: 0},
+		logger: utils.NewLogger("Node"),
+	}
+}
+
+func newClient(address string, port string) service.ServiceClient {
+	conn, err := grpc.Dial(address+":"+port)
+	if err != nil {
+		Node.logger.ErrorLogger.Fatalf("Could not connect to server. :: %v", err)
+	}
+	return service.NewServiceClient(conn)
+}
+
+func newServer(port string) service.ServiceServer {
+	listener, err := net.Listen("tcp", ":"+port)
+	if err != nil {
+		Node.logger.ErrorLogger.Fatalf("Could not listen at port %v. :: %v", port, err)
+	}
+
+	var grpcServer = grpc.NewServer()
+	service.RegisterServiceServer(grpcServer, )
 }
