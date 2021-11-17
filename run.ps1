7fe727b47c09e734124dbaf0701c65e92eaa3428
Gustav Metnik-Beck <gustav.mb@outlook.com>
2021-11-17 11:42:47 +0100

    Updated script for automatic node creation

--- run.ps1
+++ run.ps1
@@ -3,22 +3,38 @@ param([Int32]$n=3)
 #go run . -name node1 -address 127.0.0.1 -sport 8080 -bport 8081 -cport 8081
 #go run . -name node2 -address 127.0.0.1 -sport 8082 -bport 8083 -cport 8081
 
-$names = @('node0', 'node1', 'node2', 'node3')
-$sports = @(8080, 8081, 8082, 8083)
-$ips = @(@('8081,8082,8083'), @('8080,8082,8083'), @('8080,8081,8083'), @('8080,8081,8082'))
+$names = @('node0', 'node1', 'node2', 'node3', 'node4')
+$sports = @('8080', '8081', '8082', '8083', '8084')
+$ips = ''
+$delays = @(5, 5, 5, 5, 5)
 
 for ($i = 0; $i -lt $n; $i++) {
-    $name = $i
-
+    $ips = ''
     if ($i -lt $names.count) {
         $name = $names[$i]
         $sport = $sports[$i]
-        $ip = $ips[$i]
+        $delay = $delays[$i]
+        
+        for ($j = 0; $j -lt $n; $j++) {
+            
+            if ($n -ne 1) {
+                if ($sports[$j] -eq $sport) {
+                    continue
+                }
+            }
+
+            if ($ips -eq '') {
+                $ips += $sports[$j]
+            } else {
+                $ips += ',' + $sports[$j]
+            }
+        }
     } else {
-        "WARNING: Max number of nodes for this script has been created."
-        "Not creating any more nodes!"
+        "WARNING: Max number of nodes for this script has been created (" + $names.count + "). => Not creating any more nodes!"
         break
     }
+
+    "STARTING GO => $name | Server port: $sport | IPs: $ips | Delay: $delay |"
 
     $Command = 'cmd /c start powershell -NoExit -Command {
             $host.UI.RawUI.WindowTitle = "Node - ' + $name + '";
@@ -26,7 +42,7 @@ for ($i = 0; $i -lt $n; $i++) {
             $host.UI.RawUI.ForegroundColor = "white";
             Clear-Host;
             cd node;
-            go run . -name ' + $name + ' -sport ' + $sport + ' -ips ' + $ip + ';
+            go run . -name ' + $name + ' -sport ' + $sport + ' -ips ' + $ips + ' -delay ' + $delay + ';
         }'
 
     invoke-expression -Command $Command
